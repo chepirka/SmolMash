86b85ebacc4b9193eaf1193ff0e5b46ce101ed4f
HomePC <denisgolubovich67gd@gmail.com>
2024-04-16 17:53:08 +0300

    1

--- Smolmash/action.php
+++ Smolmash/action.php
@@ -6,27 +6,42 @@
     require 'includes/PHPMailer/src/Exception.php';
     require 'includes/PHPMailer/src/PHPMailer.php';
     require 'includes/PHPMailer/src/SMTP.php';
+
+    $name = $_POST['name'];
+    $phone =   $_POST['phone'];
+    $email =   $_POST['email'];
+    $city =   $_POST['city'];
+    $school =   $_POST['school'];
+    $class =   $_POST['class'];
     
     $mail = new PHPMailer();
     $mail->isSMTP();                   // Отправка через SMTP
     $mail->Host   = 'smtp.yandex.ru';  // Адрес SMTP сервера
     $mail->SMTPAuth   = true;   
-    $mail->Username   = 'Shamar.Jar';       // ваше имя пользователя (без домена и @)
-    $mail->Password   = 'yboeiorclxgsnrdz';    // ваш пароль
+    $mail->Username   = 'dgfpupkin';       // ваше имя пользователя (без домена и @)
+    $mail->Password   = 'tuvzxyeqthqdsxzu';    // ваш пароль
     $mail->SMTPSecure = 'SSL';         // шифрование ssl
     $mail->Port   = 587;               // порт подключения
      
-    $mail->setFrom('Shamar.Jar@yandex.ru', 'Конкурс Смолмаш');    // от кого
-    $mail->addAddress('Shamar.Jar@yandex.ru', 'Участник записался'); // кому
+    $mail->setFrom('dgfpupkin@yandex.ru', 'Конкурс Смолмаш');    // от кого
+    $mail->addAddress('dgfpupkin@yandex.ru', 'Участник записался'); // кому
      
     $mail->Subject = 'Участник записался';
     $mail->msgHTML("<html><body>
                     <h1>Здравствуйте!</h1>
-                    <p>Это тестовое письмо.</p>
+                    <p>ФИО: $name<br>
+                      Телефон: $phone<br>
+                      Почта: $email<br>
+                      Город: $city<br>
+                      Школа: $school<br>
+                      Класс: $class
+                    </p>
                     </html></body>");
     // Отправляем
     if ($mail->send()) {
-      echo 'Письмо отправлено!';
+      header('location: thank-you.html');
+      sleep(1);
+      header('location: index.html');
     } else {
       echo 'Ошибка: ' . $mail->ErrorInfo;
     }
