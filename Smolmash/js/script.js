99fabbd91de99aa38d5a4a86edbeb33cd961d712
Jorik00 <Jaros_Shad@mail.ru>
2024-05-29 18:21:54 +0300

    111

--- Smolmash/js/script.js
+++ Smolmash/js/script.js
@@ -39,8 +39,8 @@ jQuery(document).ready(function () {
 function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
     const body = document.body;
-    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
-    if (navLinks.style.display === 'block') {
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    if (navLinks.style.display === 'flex') {
         body.classList.add('no-scroll');
     } else {
         body.classList.remove('no-scroll');
